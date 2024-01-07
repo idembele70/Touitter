a01fa6f38010383bdfc250ae588947fd9f8ccc9f
idembele70 <idembele70@gmail.com>
2024-01-08 00:35:17 +0100

    feat: #1: adding a form builder to sign-up page

--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent{
+  public signUpForm: FormGroup<{
+    pseudo: FormControl<string | null>,
+    email: FormControl<string | null>,
+    password: FormControl<string | null>,
+  }> = this.formBuilder.group(
+    {
+      pseudo: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,}$/i)]]
+    })
+  private destroyed: Subject<boolean> = new Subject<boolean>()
 
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+  /**
+   * onSubmit
+   */
+  public onSubmit() {
+    console.log('handle Submit')
+  }
+  
 }
